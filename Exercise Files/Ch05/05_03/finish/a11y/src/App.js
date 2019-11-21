763c80d89386a44945509b59259359be62334094
rashtell <rashtell2@gmail.com>
2019-11-21 03:08:10 +0100

    Ex_Files_React_Components

---
+++ Exercise Files/Ch05/05_03/finish/a11y/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render() {
+    return (
+      <header>
+        <a href="#home" />
+        <img src="/img/is-an-image.jpg" alt="" />
+      </header>
+    )
+  }
+}
+
+export default App
---
+++ Exercise Files/Ch05/05_03/start/a11y/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <a href="#home"></a>
+        <img src="/img/is-an-image.jpg" alt=""/>
+      </div>
+    );
+  }
+}
+
+export default App;
